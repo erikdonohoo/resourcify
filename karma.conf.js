f7d483cebe63c8c512eb3b0450a58e6c490e4691
Erik Donohoo <erikdonohoo@gmail.com>
2014-11-14 07:19:20 -0700

    Use spaces instead of tabs

--- karma.conf.js
+++ karma.conf.js
@@ -2,55 +2,55 @@
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
 module.exports = function(config) {
-	'use strict';
-	config.set({
-		// base path, that will be used to resolve files and exclude
-		basePath: '',
+  'use strict';
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-		// testing framework to use (jasmine/mocha/qunit/...)
-		frameworks: ['jasmine'],
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
 
-		// list of files / patterns to load in the browser
-		// You must import all required files here as they won't be inserted automatically
-		files: [
-			'bower_components/angular/angular.js',
-			'bower_components/angular-mocks/angular-mocks.js',
-			'src/**/*.js',
-			'test/mock/**/*.js',
-			'test/spec/**/*.js'
-		],
+    // list of files / patterns to load in the browser
+    // You must import all required files here as they won't be inserted automatically
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js'
+    ],
 
-		// list of files / patterns to exclude
-		exclude: [],
+    // list of files / patterns to exclude
+    exclude: [],
 
-		// web server port
-		port: 8081,
+    // web server port
+    port: 8081,
 
-		// level of logging
-		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-		logLevel: config.LOG_INFO,
-
-
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: false,
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
-		browsers: ['PhantomJS'],
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-		// Continuous Integration mode
-		// if true, it capture browsers, run tests and exit
-		singleRun: false,
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
 
-		// How long does Karma wait for a message from a browser before disconnecting it (in ms).
-		browserNoActivityTimeout: 100000 /* no matter how high you set it, it'll max to 86 seconds... weird */
-	});
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+    // How long does Karma wait for a message from a browser before disconnecting it (in ms).
+    browserNoActivityTimeout: 100000 /* no matter how high you set it, it'll max to 86 seconds... weird */
+  });
 };
--- src/main.js
+++ src/main.js
@@ -4,29 +4,29 @@ var resourcify = angular.module('resourcify', []);
 
 function resourcificator ($http, $q) {
 
-	var $resourcifyError = angular.$$minErr('resourcify'),
-			requestOptions = ['query', 'get', '$get', '$save', '$update', '$delete'],
-			requestMethods = {
-				'query': 'GET',
-				'get': 'GET',
-				'$get': 'GET',
-				'$save': 'POST',
-				'$update': 'PUT',
-				'$delete': 'DELETE'
-			},
-			validMethods = ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
-			bodyMethods = ['$save', '$update', '$delete', 'PUT', 'POST', 'DELETE', 'PATCH'];
+  var $resourcifyError = angular.$$minErr('resourcify'),
+      requestOptions = ['query', 'get', '$get', '$save', '$update', '$delete'],
+      requestMethods = {
+        'query': 'GET',
+        'get': 'GET',
+        '$get': 'GET',
+        '$save': 'POST',
+        '$update': 'PUT',
+        '$delete': 'DELETE'
+      },
+      validMethods = ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
+      bodyMethods = ['$save', '$update', '$delete', 'PUT', 'POST', 'DELETE', 'PATCH'];
 
-	function validMethod (method) {
-		if (!~validMethods.indexOf(method)) {
-			throw $resourcifyError('requesttype', '"@{0}" is not a valid request method.', method);
-		}
-		return method;
-	}
+  function validMethod (method) {
+    if (!~validMethods.indexOf(method)) {
+      throw $resourcifyError('requesttype', '"@{0}" is not a valid request method.', method);
+    }
+    return method;
+  }
 
-	function replaceParams (params, url) {
-		
-	}
+  function replaceParams (params, url) {
+    
+  }
 }
 
 resourcificator.$inject = ['$http', '$q'];
