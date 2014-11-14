421f35098e1652957d99bb0c64693c6324322e89
Erik Donohoo <erikdonohoo@gmail.com>
2014-11-14 13:34:48 -0700

    Finalize and test utils

--- src/main.js
+++ src/main.js
@@ -2,20 +2,60 @@
 
 var resourcify = angular.module('resourcify', []);
 
-function resourcifyUtils ($log) {
+function resourcifyUtils () {
 
   // Strip off any query params, and put them in a params object
   function objectifyQueryParams (url) {
     var params = {}, query = url.substring(url.indexOf('?'));
-    $log.debug(query);
+    if (query !== url) {
+      query.replace(
+          new RegExp('([^?=&]+)(=([^&]*))?', 'g'),
+          function($0, $1, $2, $3) { params[$1] = $3; }
+      );
+    }
     return params;
   }
 
   // Finds and replaces query params and path params
-  function replaceParams (params, url) {
-    var findParam = /[\/=](:\w*[a-zA-Z]\w*)/g, copiedPath = angular.copy(url);
-    params = angular.extend({}, params, objectifyQueryParams(copiedPath));
-    $log.debug(findParam, url, params);
+  function replaceParams (params, url, object) {
+    var findParam = /[\/=](:\w*[a-zA-Z]\w*)/, copiedPath = angular.copy(url), match, cut = '__|cut|__';
+    object = object || {};
+
+    // Pull off query
+    var qParams = objectifyQueryParams(copiedPath), finalParams = {};
+    if (copiedPath.indexOf('?') !== -1) {
+      copiedPath = copiedPath.substring(0, copiedPath.indexOf('?'));
+    }
+
+    angular.forEach(qParams, function (value, key) {
+      var pseudoKey = value.substring(1);
+      if (value.charAt(0) === ':' && (params[pseudoKey] || object[pseudoKey])) {
+        finalParams[key] = params[pseudoKey] || object[pseudoKey];
+        delete params[pseudoKey]; // Don't re-use param as query param if it filled one
+      } else if (value.charAt(0) !== ':') {
+        finalParams[key] = value;
+      }
+    });
+
+    while ((match = findParam.exec(copiedPath))) {
+      var regexVal = match[1], key = match[1].substring(1);
+      copiedPath = copiedPath.replace(regexVal, params[key] || object[key] || cut);
+      if (params[key]) {
+        delete params[key];
+      } else if (copiedPath.indexOf('/' + cut) !== -1) {
+        copiedPath = copiedPath.substring(0, copiedPath.indexOf('/' + cut));
+        break;
+      }
+    }
+
+    // Add on query
+    params = angular.extend({}, finalParams, params);
+    var stringParams = [];
+    angular.forEach(params, function (value, key) {
+      stringParams.push(key + '=' + value);
+    });
+    copiedPath += ((stringParams.length) ? '?' : '') + stringParams.join('&');
+
     return copiedPath;
   }
 
@@ -24,8 +64,6 @@ function resourcifyUtils ($log) {
     replaceParams: replaceParams
   };
 }
-
-resourcifyUtils.$inject = ['$log'];
 
 resourcify.factory('resourcifyUtils', resourcifyUtils);
 
--- test/spec/utils.js
+++ test/spec/utils.js
@@ -3,27 +3,100 @@
 // This is just an example, you can delete
 describe('Service: ResourceUtils', function () {
 
-	beforeEach(module('resourcify'));
+  beforeEach(module('resourcify'));
 
-	var utils;
+  var replaceParams, objectify;
 
-	// Init
-	beforeEach(inject(function(_resourcifyUtils_){
-		utils = _resourcifyUtils_;
-	}));
+  // Init
+  beforeEach(inject(function(_resourcifyUtils_){
+    replaceParams = _resourcifyUtils_.replaceParams;
+    objectify = _resourcifyUtils_.objectifyQueryParams;
+  }));
 
-	it('should pull off query params', function () {
-		expect(utils.objectifyQueryParams('http://blah.com/stuff?who=me&you=me')).toEqual({
-			'who': 'me',
-			'you': 'me'
-		});
-		expect(utils.objectifyQueryParams('http://localhost:8080/my/friend/bob')).toEqual({});
-		expect(utils.objectifyQueryParams('http://localhost/dude?')).toEqual({});
-		expect(utils.objectifyQueryParams('http://blah.com?bro=:me&dude=:you&so=what'))
-		.toEqual({
-			'bro': ':me',
-			'dude': ':you',
-			'so': 'what'
-		});
-	});
+  describe('objectifyQueryParams', function () {
+
+    it('should turn query params into object', function () {
+      expect(objectify('http://blah.com/stuff?who=me&you=me')).toEqual({
+        'who': 'me',
+        'you': 'me'
+      });
+    });
+
+    it('should return {} with no query params', function () {
+      expect(objectify('http://localhost:8080/my/friend/bob')).toEqual({});
+      expect(objectify('http://localhost/dude?')).toEqual({});
+    });
+
+    it('should handle variable parameters in query', function () {
+      expect(objectify('http://blah.com?bro=:me&dude=:you&so=what'))
+      .toEqual({
+        'bro': ':me',
+        'dude': ':you',
+        'so': 'what'
+      });
+    });
+  });
+
+  describe('replaceParams', function () {
+
+    it('should handle a url with no matching needed', function () {
+      expect(replaceParams({}, 'http://localhost/api/v1/go', {}))
+      .toEqual('http://localhost/api/v1/go');
+    });
+
+    it('should handle a value supplied with passed params', function () {
+      expect(replaceParams({key: 'value'}, 'http://localhost/api/:key', {}))
+      .toEqual('http://localhost/api/value');
+    });
+
+    it('should handle a missing body', function () {
+      expect(replaceParams({key: 'value'}, 'http://localhost/api/:key'))
+      .toEqual('http://localhost/api/value');
+    });
+
+    it('should prefer a passed param to one on the body', function () {
+      expect(replaceParams({key: 'value'}, 'http://localhost/api/:key', {key: 'value2'}))
+      .toEqual('http://localhost/api/value');
+    });
+
+    it('should preserve supplied query params', function () {
+      expect(replaceParams({key: 'value'}, 'http://localhost/api/:key?dude=dude'))
+      .toEqual('http://localhost/api/value?dude=dude');
+    });
+
+    it('should strip query params that have no supplied value from object', function () {
+      expect(replaceParams({}, 'http://localhost/api?query=:friend&query2=:amigo', {amigo: 'bob'}))
+      .toEqual('http://localhost/api?query2=bob');
+    });
+
+    it('should strip query params that have no supplied value from params', function () {
+      expect(replaceParams({amigo: 'bob'}, 'http://localhost/api?query=:friend&query2=:amigo'))
+      .toEqual('http://localhost/api?query2=bob');
+    });
+
+    it('should handle a mix of supplied and variable query params', function () {
+      expect(replaceParams({amigo: 'bob'}, 'http://localhost/api?query=:friend&query2=bro&query3=:amigo'))
+      .toEqual('http://localhost/api?query2=bro&query3=bob');
+    });
+
+    it('should prefer passed params when filling query parameters', function () {
+      expect(replaceParams({one: '1'}, 'http://localhost/api?query=:one&friend=steve', {one: '2'}))
+      .toEqual('http://localhost/api?query=1&friend=steve');
+    });
+
+    it('should handle multiple parameters', function () {
+      expect(replaceParams({}, 'http://localhost/api/:version/thing/:thingId/bob', {version: 'v1', thingId: '123', action: 'post'}))
+      .toEqual('http://localhost/api/v1/thing/123/bob');
+    });
+
+    it('should handle multiple parameters while ending with a param', function () {
+      expect(replaceParams({}, 'http://localhost/api/:version/thing/:thingId/bob/:action', {version: 'v1', thingId: '123', action: 'post'}))
+      .toEqual('http://localhost/api/v1/thing/123/bob/post');
+    });
+
+    it('should not use body values as query parameters if no match is found', function () {
+      expect(replaceParams({}, 'http://localhost/api/:version', {version: 'v1', thingId: '123', action: 'post'}))
+      .toEqual('http://localhost/api/v1');
+    });
+  });
 });
