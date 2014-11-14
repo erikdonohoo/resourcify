395270cca12886e2694eaa5d7787062dece40662
Erik Donohoo <erikdonohoo@gmail.com>
2014-11-13 19:40:22 -0700

    Start the awesome

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
 'use strict';
-angular.module('resourcify', [])
 
-// This is just an example service you should delete
-// You should put all files (directives/services/filters, etc) in their own file
-.service('stub', [function Stub() {
+var resourcify = angular.module('resourcify', []);
 
-	Stub.prototype.getOne = function () {
-		return 1;
-	};
+function resourcificator ($http, $q) {
 
-}]);
+	var $resourcifyError = angular.$$minErr('resourcify'),
+			requestOptions = ['query', 'get', '$get', '$save', '$update', '$delete'],
+			requestMethods = {
+				'query': 'GET',
+				'get': 'GET',
+				'$get': 'GET',
+				'$save': 'POST',
+				'$update': 'PUT',
+				'$delete': 'DELETE'
+			},
+			validMethods = ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
+			bodyMethods = ['$save', '$update', '$delete', 'PUT', 'POST', 'DELETE', 'PATCH'];
+
+	function validMethod (method) {
+		if (!~validMethods.indexOf(method)) {
+			throw $resourcifyError('requesttype', '"@{0}" is not a valid request method.', method);
+		}
+		return method;
+	}
+
+	function replaceParams (params, url) {
+		
+	}
+}
+
+resourcificator.$inject = ['$http', '$q'];
+
+resourcify.service('resourcify', resourcificator);
